28ffb38ae7da72f4a66b1a21f227d602d379feec
sirkells <kelechi_igbokwe@yahoo.com>
2019-03-03 14:54:03 +0100

    finished components nesting with custom children

--- components/src/index.js
+++ components/src/index.js
@@ -2,29 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Faker from "faker";
 import CommentComp from "./CommentComp";
+import Permit from "./Permit";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentComp
-        name="Kells"
-        timeAdded="Today at 6:00PM"
-        avatar={Faker.image.avatar()}
-        comments="Nice Blog Post"
-      />
-      <CommentComp
-        name="James"
-        timeAdded="Today at 5:20PM"
-        avatar={Faker.image.avatar()}
-        comments="Amazing post, would love more"
-      />
-      <CommentComp
-        name="Tony"
-        timeAdded="Yesterday at 3:00PM"
-        avatar={Faker.image.avatar()}
-        comments="This will be great for business reports. I will definitely download
-        this."
-      />
+      <Permit>
+        <CommentComp
+          name="Kells"
+          timeAdded="Today at 6:00PM"
+          avatar={Faker.image.avatar()}
+          comments="Nice Blog Post"
+        />
+      </Permit>
+      <Permit>
+        <CommentComp
+          name="James"
+          timeAdded="Today at 5:20PM"
+          avatar={Faker.image.avatar()}
+          comments="Amazing post, would love more"
+        />
+      </Permit>
+      <Permit>
+        <CommentComp
+          name="Tony"
+          timeAdded="Yesterday at 3:00PM"
+          avatar={Faker.image.avatar()}
+          comments="This will be great for business reports. I will definitely download
+            this."
+        />
+      </Permit>
     </div>
   );
 };
