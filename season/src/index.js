fe643ef5ea6177f4db5b0b1407855be0fdd5bf67
sirkells <kelechi_igbokwe@yahoo.com>
2019-03-04 09:56:57 +0100

    added state

--- season/src/index.js
+++ season/src/index.js
@@ -1,12 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    err => console.log(err)
-  );
-  return <div>Hi there</div>;
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null, errorMsg: "" };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position =>
+        this.setState({
+          lat: position.coords.latitude
+        }),
+      err => {
+        this.setState({
+          errorMsg: err.message
+        });
+      }
+    );
+  }
+
+  render() {
+    if (this.state.errorMsg && !this.state.lat) {
+      return <div>Error: {this.state.errorMsg}</div>;
+    }
+    if (this.state.lat && !this.state.errorMsg) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+    return <div>Loading....</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
